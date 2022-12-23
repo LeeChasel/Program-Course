7e6e10543da82e4a53516e22ccfa3020b562eae1
Chasel <arko9988b@gmail.com>
2022-12-23 09:57:58 +0800

    add asp.net hw2

---
+++ Network Programing/HW2/HW2/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HW2.Data;
+
+namespace HW2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            StudentData db = new StudentData();
+            return View(db.GetStudents());
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Search(string id)
+        {
+            StudentData db = new StudentData();
+            return View(db.GetStudentById(id));
+        }
+    }
+}
---
+++ Network Programing/HW2/HW2/Data/StudentData.cs
@@ -0,0 +1,66 @@
+﻿using System.Data.SqlClient;
+using HW2.Models;
+
+namespace HW2.Data
+{
+    public class StudentData
+    {
+        private string strcon;
+        public StudentData() 
+        {
+            strcon = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=hw2;Integrated Security=True;Pooling=False";
+        }
+        public List<Student> GetStudents()
+        {
+            List<Student> res = new List<Student>();
+            using (SqlConnection sqlcon = new SqlConnection(strcon))
+            {
+                sqlcon.Open();
+                using (SqlCommand sqlcmd = new SqlCommand())
+                {
+                    sqlcmd.Connection = sqlcon;
+                    sqlcmd.CommandText = "select * from Student";
+                    SqlDataReader sqldr = sqlcmd.ExecuteReader();
+                    while (sqldr.Read())
+                    {
+                        res.Add(new Student()
+                        {
+                            Id = sqldr.GetInt32(0),
+                            Name = sqldr.GetString(1),
+                            Department= sqldr.GetString(2)
+                        });
+                    }
+                }
+                sqlcon.Close();
+            }
+            return res;
+        }
+
+        public List<Student> GetStudentById(string id)
+        {
+            List<Student> res = new List<Student>();
+            using (SqlConnection sqlcon = new SqlConnection(strcon)) 
+            {
+                sqlcon.Open();
+                using (SqlCommand sqlcmd = new SqlCommand())
+                {
+                    sqlcmd.Connection = sqlcon;
+                    sqlcmd.CommandText = "select * from Student where ID=@i";
+                    sqlcmd.Parameters.AddWithValue("@i", id);
+                    SqlDataReader sqldr = sqlcmd.ExecuteReader();
+                    while (sqldr.Read())
+                    {
+                        res.Add(new Student()
+                        {
+                            Id = sqldr.GetInt32(0),
+                            Name = sqldr.GetString(1),
+                            Department = sqldr.GetString(2)
+                        });
+                    }
+                }
+                sqlcon.Close();
+            }
+            return res;
+        }
+    }
+}
